cf6565f6163cce3ee8114e0084a0fdba0ad67e15
onurmumcu <onur.mmc@gmail.com>
2019-09-22 17:56:57 -0400

    dsffadf

--- src/test/java/TestCases/HomePageTest.java
+++ src/test/java/TestCases/HomePageTest.java
@@ -2,6 +2,7 @@ package TestCases;
 
 import java.io.IOException;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import pageObjects.LandingPage;
@@ -11,12 +12,14 @@ import resources.Base;
 public class HomePageTest extends Base{
 	
 	@Test
-	public void basePageNavigation() throws IOException {
+	public void basePageNavigation() throws IOException, InterruptedException {
 		driver=initializeDriver();
 		driver.get("http://qaclickacademy.com");
 		driver.manage().window().maximize();
 		
 		LandingPage l = new LandingPage(driver);
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("//button[contains(text(),'NO THANKS')]")).click();
 		l.getLogin().click();
 		
 		LoginPage lp = new LoginPage(driver);
